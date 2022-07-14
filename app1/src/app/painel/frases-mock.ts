cb5d69371c15c0b392dd18a51207a59eee952850
Emilly Roberta <emilly.roberta@ambevtech.com.br>
2022-07-14 15:42:22 -0300

    Projeto de tradução

---
+++ app1/src/app/painel/frases-mock.ts
@@ -0,0 +1,8 @@
+import { Frase } from '../shared/frase.model'
+
+export const FRASES: Array<Frase> = [
+    {fraseEng: 'I like to learn', frasePtBr: 'Eu gosto de aprender'},
+    {fraseEng: 'How are you?', frasePtBr: 'Como vai voce?'},
+    {fraseEng: 'I watch TV', frasePtBr: 'Eu assisto TV'},
+    {fraseEng: 'I eat bread', frasePtBr: 'I eat bread'}
+]
