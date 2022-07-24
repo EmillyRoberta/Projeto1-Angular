2df38e353cbc8fb14cc864c3ebfffe09c82c2e75
Emilly Roberta <emilly.roberta@ambevtech.com.br>
2022-07-24 00:03:21 -0300

    mapeando as ofertas

--- app2/src/app/oferta/oferta.component.ts
+++ app2/src/app/oferta/oferta.component.ts
@@ -1,21 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { OfertasService } from '../ofertas.service';
+import { Oferta } from '../shared/oferta.model';
 
 @Component({
   selector: 'app-oferta',
   templateUrl: './oferta.component.html',
-  styleUrls: ['./oferta.component.scss']
+  styleUrls: ['./oferta.component.scss'],
+  providers: [ OfertasService ]
 })
 export class OfertaComponent implements OnInit {
 
+  public oferta:Array<Oferta> = []
 
-  constructor(private route:ActivatedRoute) { }
+
+  constructor(private route:ActivatedRoute, private ofertasService:OfertasService) { }
 
   ngOnInit(): void {
-    this.route.snapshot.params['id'] 
+    
     // this.route.params.subscribe((parametro:any)=>{ //quando altera a rota, ele executa uma acao 
     //   console.log(parametro.id)
     // })
+    this.ofertasService.getOfertasPorId(this.route.snapshot.params['id'])
+    .subscribe((res)=>{
+      this.oferta = res
+
+    })
+
   }
+
 
 }
--- app2/src/app/ofertas.service.ts
+++ app2/src/app/ofertas.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Oferta } from './shared/oferta.model';
+import { URL_API } from './app.api'
 
 @Injectable()
 export class OfertasService{
@@ -9,12 +9,16 @@ export class OfertasService{
     constructor(private http:HttpClient){}
  
     public getOfertas():Observable<any>{
-       return this.http.get('http://localhost:3000/ofertas?destaque=true')
+       return this.http.get(`${URL_API}?destaque=true`)
         
     }
 
     public getOfertarPorCategoria(categoria: string):Observable<any>{
-        return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+        return this.http.get(`${URL_API}?categoria=${categoria}`)
+    }
+
+    public getOfertasPorId(id:number):Observable<any>{
+        return this.http.get(`${URL_API}?id=${id}`)
     }
 
 }
